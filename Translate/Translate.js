c02f181d09eba0ab7dfd9160848d1c0232c8151e
ToxesFoxes <toxes_foxes@outlook.com>
2020-09-15 19:13:45 +0600

    Updated Languages and Translations

--- Translate/Translate.js
+++ Translate/Translate.js
@@ -5,32 +5,21 @@ LIBRARY({
 	api: "CoreEngine"
 });
 
-
-var firstStart = __config__.access("firstStart");
-if (firstStart == null) __config__.set("firstStart", true);
-__config__.save();
-firstStart = __config__.getBool("firstStart");
-if (firstStart) {
-	alert(Translate("Translations may be inaccurate"));
-	alert(Translate("Loaded Your Language: English - Default"));
-	__config__.set("firstStart", false);
-}
 function Translate(_string, _isPath) {
 	if (!_string) return;
-	var langs = ["en", "nl", "de", "es", "fr", "it", "ja", "ko", "pt", "ru", "zh"];
-	// English, Nederlands, Deutsch, Espanol, Français, Italiano, Japanese, Korean, Português, Russian, Chinese 
+	var langs = ["bg", "cs", "da", "de", "el", "en", "es", "fi", "fr", "hu", "id", "it", "ja", "ko", "nb", "nl", "pl", "pt", "ru", "sk", "sv", "tr", "uk", "zh"];
+	// 	Bulgaria, Czech, Denmark, Deutsch, Greece, English, Espanol, Finland, Français, Hungary, Indonesia, Italiano, Japanese, Korean, Norway, Nederlands, Poland, Português, Russian, Slovakia, Sweden, Turkey, Ukraine, Chinese
 	if (_isPath) {
-		eval('var file=' + FileTools.ReadText(__dir__ + _string))
+		eval('var file=' + FileTools.ReadText(__dir__ + _string));
 		for (var str in file) {
 			let langList = file[str], translations = {};
 			for (let i in langs) {
 				let lang = langs[i];
 				if (langList[lang]) translations[lang] = langList[lang];
 			}
-			Translation.addTranslation(str, translations)
+			Translation.addTranslation(str, translations);
 		}
 	} else return Translation.translate(_string);
 }
-Translate('assets/translations.json', true);
 
 EXPORT("Translate", Translate);
