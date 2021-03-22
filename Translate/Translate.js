e346a3b89dfb37d2ab0f74427bebe65283eaf52a
ToxesFoxes <toxes_foxes@outlook.com>
2021-03-22 20:38:53 +0600

    Oh yeah, some update

--- Translate/Translate.js
+++ Translate/Translate.js
@@ -1,25 +1,72 @@
 LIBRARY({
 	name: "Translate",
-	version: 1,
+	version: 2,
 	shared: false,
 	api: "CoreEngine"
-});
+})
+/**
+ * Lang codes of the languages that MCPE uses
+ */
+const langs = [
+	"bg", /* Bulgaria   | Bulgarian          */
+	"cs", /* Czech      | Czech              */
+	"da", /* Denmark    | Danish             */
+	"de", /* Deutsch    | German             */
+	"el", /* Greece     | Greek              */
+	"en", /* English    | English            */
+	"es", /* Espanol    | Spanish            */
+	"fi", /* Finland    | Finnish            */
+	"fr", /* Français   | French             */
+	"hu", /* Hungary    | Hungarian          */
+	"id", /* Indonesia  | Indonesian         */
+	"it", /* Italiano   | Italian            */
+	"ja", /* Japanese   | Japanese           */
+	"ko", /* Korean     | Korean             */
+	"nb", /* Norway     | Norwegian          */
+	"nl", /* Nederlands | Dutch              */
+	"pl", /* Poland     | Polish             */
+	"pt", /* Português  | Portuguese         */
+	"ru", /* Russian    | Russian            */
+	"sk", /* Slovakia   | Slovak             */
+	"sv", /* Sweden     | Swedish            */
+	"tr", /* Turkey     | Turkish            */
+	"uk", /* Ukraine    | Ukrainian          */
+	"zh"  /* Chinese    | Chinese Simplified */
+]
 
-function Translate(_string, _isPath) {
-	if (!_string) return;
-	var langs = ["bg", "cs", "da", "de", "el", "en", "es", "fi", "fr", "hu", "id", "it", "ja", "ko", "nb", "nl", "pl", "pt", "ru", "sk", "sv", "tr", "uk", "zh"];
-	// 	Bulgaria, Czech, Denmark, Deutsch, Greece, English, Espanol, Finland, Français, Hungary, Indonesia, Italiano, Japanese, Korean, Norway, Nederlands, Poland, Português, Russian, Slovakia, Sweden, Turkey, Ukraine, Chinese
-	if (_isPath) {
-		eval('var file=' + FileTools.ReadText(__dir__ + _string));
-		for (var str in file) {
-			let langList = file[str], translations = {};
-			for (let i in langs) {
-				let lang = langs[i];
-				if (langList[lang]) translations[lang] = langList[lang];
-			}
-			Translation.addTranslation(str, translations);
+/**
+ * Reads normal json with comments
+ * @param {string} path - absolute path to file
+ */
+function ReadJson(path) {
+	eval('var file=' + FileTools.ReadText(path))
+	return file
+}
+/**
+ * Loading and translation all values
+ * @param {string} path - absolute path to file
+ */
+function LoadTranslationFile(path) {
+	var file = ReadJson(path)
+	for (var str in file) {
+		let langList = file[str], translations = {}
+		for (let i in langs) {
+			let lang = langs[i]
+			if (langList[lang]) translations[lang] = langList[lang]
 		}
-	} else return Translation.translate(_string);
+		Translation.addTranslation(str, translations)
+	}
 }
 
-EXPORT("Translate", Translate);
+function Translate(str) {
+	return Translation.translate(str)
+}
+
+var API = {
+	Translate: Translate,
+	LoadTranslationFile: LoadTranslationFile,
+	langs: langs,
+	ReadJson: ReadJson
+}
+
+EXPORT("Translate", API)
